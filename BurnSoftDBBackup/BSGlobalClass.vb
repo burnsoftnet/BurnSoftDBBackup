91c29b0cfa7654ba948bc8cd74cf4fcbfe3a07f8
Joe M <bs.github@burnsoft.net>
2021-03-17 14:08:51 -0400

    added comments

--- BurnSoftDBBackup/BSGlobalClass.vb
+++ BurnSoftDBBackup/BSGlobalClass.vb
@@ -1,15 +1,32 @@
-Imports System
 Imports System.IO
 Imports System.Text
-Imports System.Windows.Forms
 Imports Microsoft.Win32
+''' <summary>
+''' The GlobalClasses namespace.
+''' </summary>
+' ReSharper disable once CheckNamespace
 Namespace BurnSoft.GlobalClasses
+    ''' <summary>
+    ''' Class BSRegistry.
+    ''' </summary>
     Public Class BSRegistry
+        ''' <summary>
+        ''' The default reg path
+        ''' </summary>
         Public DefaultRegPath As String
 #Region "Registry General Functions and Subs"
+        ''' <summary>
+        ''' Creates the sub key.
+        ''' </summary>
+        ''' <param name="strValue">The string value.</param>
         Private Sub CreateSubKey(ByVal strValue As String)
             Microsoft.Win32.Registry.CurrentUser.CreateSubKey(strValue)
         End Sub
+        ''' <summary>
+        ''' Regs the sub key exists.
+        ''' </summary>
+        ''' <param name="strValue">The string value.</param>
+        ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         Private Function RegSubKeyExists(ByVal strValue As String) As Boolean
             Dim bAns As Boolean = False
             Try
@@ -25,6 +42,13 @@ Namespace BurnSoft.GlobalClasses
             End Try
             Return bAns
         End Function
+        ''' <summary>
+        ''' Gets the reg sub key value.
+        ''' </summary>
+        ''' <param name="strKey">The string key.</param>
+        ''' <param name="strValue">The string value.</param>
+        ''' <param name="strDefault">The string default.</param>
+        ''' <returns>System.String.</returns>
         Private Function GetRegSubKeyValue(ByVal strKey As String, ByVal strValue As String, ByVal strDefault As String) As String
             Dim sAns As String = ""
             Dim strMsg As String = ""
@@ -49,6 +73,10 @@ Namespace BurnSoft.GlobalClasses
             End Try
             Return sAns
         End Function
+        ''' <summary>
+        ''' Settingses the exists.
+        ''' </summary>
+        ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         Private Function SettingsExists() As Boolean
             Dim bAns As Boolean = False
             Dim MyReg As RegistryKey
@@ -62,32 +90,57 @@ Namespace BurnSoft.GlobalClasses
             End If
             Return bAns
         End Function
+        ''' <summary>
+        ''' Gets the last working dir.
+        ''' </summary>
+        ''' <returns>System.String.</returns>
         Public Function GetLastWorkingDir() As String
             Dim sAns As String = ""
             Dim strValue As String = DefaultRegPath & "\Settings"
             sAns = GetRegSubKeyValue(strValue, "LastPath", "")
             Return sAns
         End Function
+        ''' <summary>
+        ''' Gets the database path.
+        ''' </summary>
+        ''' <returns>System.String.</returns>
         Public Function GetDBPath() As String
             Dim sAns As String = ""
             sAns = GetRegSubKeyValue(DefaultRegPath, "Database", DBName)
             Return sAns
         End Function
+        ''' <summary>
+        ''' Gets the days old.
+        ''' </summary>
+        ''' <returns>System.Int64.</returns>
         Public Function GetDaysOld() As Long
             Dim lAns As Long = 0
             lAns = CLng(GetRegSubKeyValue(DefaultRegPath & "\Settings", "TrackHistoryDays", 30))
             Return lAns
         End Function
+        ''' <summary>
+        ''' Uses the tracking.
+        ''' </summary>
+        ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         Public Function UseTracking() As Boolean
             Dim bAns As Boolean = True
             bAns = CBool(GetRegSubKeyValue(DefaultRegPath & "\Settings", "TrackHistory", True))
             Return bAns
         End Function
+        ''' <summary>
+        ''' Gets the application path.
+        ''' </summary>
+        ''' <returns>System.Object.</returns>
         Public Function GetApplicationPath()
             Dim sAns As String = ""
             sAns = GetRegSubKeyValue(DefaultRegPath, "Path", Application.StartupPath) & "\"
             Return sAns
         End Function
+        ''' <summary>
+        ''' Saves the reg setting.
+        ''' </summary>
+        ''' <param name="sKey">The s key.</param>
+        ''' <param name="sValue">The s value.</param>
         Public Sub SaveRegSetting(ByVal sKey As String, ByVal sValue As String)
             Dim MyReg As RegistryKey
             Dim strValue As String = DefaultRegPath & "Settings"
@@ -98,16 +151,33 @@ Namespace BurnSoft.GlobalClasses
         End Sub
 #End Region
     End Class
+    ''' <summary>
+    ''' Class BSFileSystem.
+    ''' </summary>
     Public Class BSFileSystem
+        ''' <summary>
+        ''' Logs the file.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
+        ''' <param name="strMessage">The string message.</param>
         Public Sub LogFile(ByVal strPath As String, ByVal strMessage As String)
             Dim SendMessage As String = DateTime.Now & vbTab & strMessage
             Call AppendToFile(strPath, SendMessage)
         End Sub
+        ''' <summary>
+        ''' Deletes the file.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
         Public Sub DeleteFile(ByVal strPath As String)
             If File.Exists(strPath) Then
                 File.Delete(strPath)
             End If
         End Sub
+        ''' <summary>
+        ''' Files the exists.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
+        ''' <returns>System.Object.</returns>
         Public Function FileExists(ByVal strPath As String)
             Return File.Exists(strPath)
         End Function
@@ -117,66 +187,134 @@ Namespace BurnSoft.GlobalClasses
                 fs.Close()
             End If
         End Sub
+        ''' <summary>
+        ''' Appends to file.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
+        ''' <param name="strNewLine">The string new line.</param>
         Private Sub AppendToFile(ByVal strPath As String, ByVal strNewLine As String)
             If File.Exists(strPath) = False Then Call CreateFile(strPath)
             Dim sw As New StreamWriter(strPath, True, Encoding.ASCII)
             sw.WriteLine(strNewLine)
             sw.Close()
         End Sub
+        ''' <summary>
+        ''' Outs the put to file.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
+        ''' <param name="strNewLine">The string new line.</param>
         Public Sub OutPutToFile(ByVal strPath As String, ByVal strNewLine As String)
             Call AppendToFile(strPath, strNewLine)
         End Sub
+        ''' <summary>
+        ''' Moves the file.
+        ''' </summary>
+        ''' <param name="strFrom">The string from.</param>
+        ''' <param name="strTo">The string to.</param>
         Public Sub MoveFile(ByVal strFrom As String, ByVal strTo As String)
             If File.Exists(strFrom) Then
                 File.Move(strFrom, strTo)
             End If
         End Sub
+        ''' <summary>
+        ''' Copies the file.
+        ''' </summary>
+        ''' <param name="strFrom">The string from.</param>
+        ''' <param name="strTo">The string to.</param>
         Public Sub CopyFile(ByVal strFrom As String, ByVal strTo As String)
             If File.Exists(strFrom) Then
                 File.Copy(strFrom, strTo)
             End If
         End Sub
+        ''' <summary>
+        ''' Creates the directory.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
         Public Sub CreateDirectory(ByVal strPath As String)
             If Directory.Exists(strPath) Then
                 Directory.CreateDirectory(strPath)
             End If
         End Sub
+        ''' <summary>
+        ''' Directories the exists.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
+        ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         Public Function DirectoryExists(ByVal strPath As String) As Boolean
             Return Directory.Exists(strPath)
         End Function
+        ''' <summary>
+        ''' Deletes the directory.
+        ''' </summary>
+        ''' <param name="strPath">The string path.</param>
         Public Sub DeleteDirectory(ByVal strPath As String)
             If Directory.Exists(strPath) Then
                 Directory.Delete(strPath)
             End If
         End Sub
+        ''' <summary>
+        ''' Moves the directory.
+        ''' </summary>
+        ''' <param name="strFrom">The string from.</param>
+        ''' <param name="strTo">The string to.</param>
         Public Sub MoveDirectory(ByVal strFrom As String, ByVal strTo As String)
             If Directory.Exists(strFrom) Then
                 Directory.Move(strFrom, strTo)
             End If
         End Sub
+        ''' <summary>
+        ''' Renames the file.
+        ''' </summary>
+        ''' <param name="strFrom">The string from.</param>
+        ''' <param name="strTo">The string to.</param>
         Public Sub RenameFile(ByVal strFrom As String, ByVal strTo As String)
             File.Move(strFrom, strTo)
         End Sub
+        ''' <summary>
+        ''' Gets the path of file.
+        ''' </summary>
+        ''' <param name="strFile">The string file.</param>
+        ''' <returns>System.String.</returns>
         Public Function GetPathOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetDirectoryName(strFile)
             Return sAns
         End Function
+        ''' <summary>
+        ''' Gets the ext of file.
+        ''' </summary>
+        ''' <param name="strFile">The string file.</param>
+        ''' <returns>System.String.</returns>
         Public Function GetExtOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetExtension(strFile)
             Return sAns
         End Function
+        ''' <summary>
+        ''' Gets the name of file.
+        ''' </summary>
+        ''' <param name="strFile">The string file.</param>
+        ''' <returns>System.String.</returns>
         Public Function GetNameOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetFileName(strFile)
             Return sAns
         End Function
+        ''' <summary>
+        ''' Files the has extension.
+        ''' </summary>
+        ''' <param name="strFile">The string file.</param>
+        ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         Public Function FileHasExtension(ByVal strFile As String) As Boolean
             Dim bAns As Boolean = False
             bAns = Path.HasExtension(strFile)
             Return bAns
         End Function
+        ''' <summary>
+        ''' Gets the name of file wo ext.
+        ''' </summary>
+        ''' <param name="strFile">The string file.</param>
+        ''' <returns>System.String.</returns>
         Public Function GetNameOfFileWOExt(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetFileNameWithoutExtension(strFile)
